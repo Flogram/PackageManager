6c1752760d6556a4e4e39d041c2561ce86eedb56
didiermun <didiermunezer38@gmail.com>
2022-11-02 19:43:24 +0200

    feat: first message

--- bin/index.js
+++ bin/index.js
@@ -1,51 +1,17 @@
 #!/usr/bin/env node
 
-const yargs = require( "yargs" );
-const axios = require( "axios" );
-const chalk = require( "chalk" );
-const boxen = require( "boxen" );
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+const greeting = chalk.white.bold("Welcome to Flogram!");
+
 const boxenOptions = {
-  padding: 1,
-  margin: 0,
-  borderStyle: "round",
-  borderColor: "green"
+ padding: 1,
+ margin: 1,
+ borderStyle: "round",
+ borderColor: "green",
+ backgroundColor: "#555555"
 };
-const errBoxenOptions = {
-  padding: 1,
-  margin: 0,
-  borderStyle: "double",
-  borderColor: "red"
-};
+const msgBox = boxen( greeting, boxenOptions );
 
-const options = yargs
-  .usage( "Usage: -n <name>" )
-  .option( "n", { alias: "name", describe: "Your name", type: "string", demandOption: true } )
-  .option( "s", { alias: "search", describe: "Search term", type: "string" } )
-  .argv;
-
-const greeting = boxen( chalk.bold( `Hello, ${options.name}!` ), boxenOptions );
-console.log( greeting );
-
-if ( options.search ) {
-  console.log( `Searching for jokes about ${options.search}...` );
-} else {
-  console.log( "Here's a random joke for you:" );
-}
-
-// The url depends on searching or not
-const url = options.search ? `https://icanhazdadjoke.com/search?term=${escape( options.search )}` : "https://icanhazdadjoke.com/";
-
-axios.get( url, { headers: { Accept: "application/json" } } )
-  .then( res => {
-    if ( options.search ) {
-      // if searching for jokes, loop over the results
-      res.data.results.forEach( j => {
-        console.log( chalk.bold( "\n" + j.joke ) );
-      } );
-      if ( res.data.results.length === 0 ) {
-        console.log( boxen( chalk.red( "no jokes found :'(" ), errBoxenOptions ) );
-      }
-    } else {
-      console.log( boxen( res.data.joke, boxenOptions ) );
-    }
-  } );
+console.log(msgBox);
